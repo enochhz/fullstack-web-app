beac2da99d41cbd2dd441cc00dfa34818777b6ea
walkerzheng <walkerhaozheng@gmail.com>
2017-10-03 15:43:14 -0700

    finish base setup

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("port 5000 is listen");
